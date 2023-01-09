67bcd1d202a158b36095fdb7db70cd704aeeca95
João Victor <facebookafonsolimajoaovictor@gmail.com>
2023-01-09 20:45:24 -0300

    Tratamento de exceções e limpeza da tela implementados

--- src/application/UI.java
+++ src/application/UI.java
@@ -15,6 +15,11 @@ public class UI {
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
     public static final String ANSI_GREY_BACKGROUND = "\u001B[100m";
 
+    public static void clearScreen() {
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+    }
+
     public static ChessPosition readChessPosition(Scanner sc) {
         try {
             String s = sc.nextLine();
