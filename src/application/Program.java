09e920c5e9ed9cab32be645544116cdba85a1f49
João Victor <facebookafonsolimajoaovictor@gmail.com>
2023-01-09 14:25:13 -0300

    Impressão do tabuleiro vazio concluída

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
     public static void main(String[] args) {
 
-        System.out.println("Olá mundo!");
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
